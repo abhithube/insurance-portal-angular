b96daabb9b96275a00262b3479659618ed638862
Abhi Thube <abhimanyuthube@gmail.com>
2020-10-31 19:22:21 -0700

    Added region to AWS CLI config

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ export PATH=$PATH:$HOME/.local/bin
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 449094484520.dkr.ecr.us-west-2.amazonaws.com
 docker tag insurance-portal/angular:latest 449094484520.dkr.ecr.us-west-2.amazonaws.com/insurance-portal/angular:latest
 docker push 449094484520.dkr.ecr.us-west-2.amazonaws.com/insurance-portal/angular:latest
-aws ecs update-service --cluster insurance-portal-ec2-cluster --service angular-srv --force-new-deployment
+aws ecs update-service --cluster insurance-portal-ec2-cluster --region us-west-2 --service angular-srv --force-new-deployment
